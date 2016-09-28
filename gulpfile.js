da3c7621f8adf116028aa631428819171e740f28
Andy Becker <becker.andrew2@gmail.com>
2016-09-28 14:52:46 -0400

    Updated files with gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+	cleanCSS = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
+
+var filePaths = {
+	dev: {
+		css: './dev/css/styles.css',
+		js: './dev/js/script.js'
+	},
+	prod: {
+		css: './client_assets/css/',
+		js: './client_assets/js/'
+	}
+};
+
+gulp.task('minifyCSS', function () {
+	return gulp.src(filePaths.dev.css + '**.*')
+		.pipe(concat('styles.css'))
+		.pipe(cleanCSS({debug: true}, function(details) {
+			console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+		}))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(filePaths.prod.css));
+});
+
+gulp.task('js', function() {
+	return gulp.src(filePaths.dev.js)
+		.pipe(rename('scripts.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(filePaths.prod.js));
+});
